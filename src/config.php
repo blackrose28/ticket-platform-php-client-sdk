0e52e2f8c8f923e9a1da932f37a4eee4ee22eac1
Nguyen Anh Tuan <nguyenanhtuan02@admicro.vn>
2018-04-19 14:38:56 +0700

    ignore config url - using Env params

--- src/config.php
+++ src/config.php
@@ -1,3 +1,3 @@
 <?php
-define('GATEWAY_URL', 'http://gateway.test/shorthand_sdk.php');
-define('GATEWAYJS_URL', 'http://localhost:8080/graphql');
+//define('GATEWAY_URL', 'http://gateway.test/shorthand_sdk.php');
+//define('GATEWAYJS_URL', 'http://localhost:8080/graphql');
